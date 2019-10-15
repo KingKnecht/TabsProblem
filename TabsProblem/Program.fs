b3216092e9f328c600b3b8b5cd4b85a74efa9832
KingKnecht <kingknecht@googlemail.com>
2019-10-15 22:06:19 +0200

    Identify tabs by Id and everthing is fine :-)

--- TabsProblem/Program.fs
+++ TabsProblem/Program.fs
@@ -13,59 +13,64 @@ let createTabId = makeNameGenerator (sprintf "-%03i")
 
 type Tab =
     {
+        Id: Guid
         Name: string
     }
 
 type Model =
     {
         Tabs: Tab list
-        SelectedTab: Tab option
+        SelectedTab: Guid option
     }
 
 let init =
     {
         Tabs = []
         SelectedTab = None
-    }, Cmd.none
+    }
 
 type Msg =
     | AddTab
-    | CloseTab of Tab option
-    | CloseTab2 of Tab
-    | SetSelectedTab of Tab option
+    | CloseTab of tabId: Guid
+    | SelectTab of tabId: Guid option
 
 let update msg m =
     match msg with
     | AddTab ->
-        { m with Tabs = m.Tabs |> List.append [ { Name = "Tab" + createTabId () } ] },
-            Cmd.none
-    | CloseTab tabToClose ->
-        match tabToClose with
-        | Some tabToClose -> { m with SelectedTab = None }, Cmd.ofMsg (CloseTab2 tabToClose)
-        | None -> m, Cmd.none
-    | CloseTab2 tabToClose ->
-        { m with Tabs = m.Tabs |> List.filter ((<>) tabToClose) }, Cmd.none
-    | SetSelectedTab t ->
-        { m with SelectedTab = t }, Cmd.none
+        let newTab = { Id = Guid.NewGuid (); Name = "Tab" + createTabId () }
+        { m with Tabs = m.Tabs |> List.append [newTab] }
+    | CloseTab tabId ->
+        let newTabs = m.Tabs |> List.filter (fun t -> t.Id <> tabId)
+        // Logic for selected tab: Keep existing selected if not removed. If
+        // selected tab is removed, select next tab in list, or the new last tab
+        // if we removed the last tab, or no tab if empty
+        let newSelectedTabId =
+          if m.SelectedTab <> Some tabId then m.SelectedTab
+          else
+            let selectedIdx = m.Tabs |> List.findIndex (fun t -> t.Id = tabId)
+            newTabs
+            |> List.tryItem selectedIdx
+            |> Option.orElse (newTabs |> List.tryLast)
+            |> Option.map (fun t -> t.Id)
+        { m with Tabs = newTabs; SelectedTab = newSelectedTabId }
+    | SelectTab t ->
+        { m with SelectedTab = t }
 
 let bindings () : Binding<Model, Msg> list =
     [
         "Tabs" |> Binding.subModelSeq((fun m -> m.Tabs), (fun s -> s), fun () ->
             [
-                "Tab" |> Binding.oneWay (fun (_, e) -> e)
-                "CloseTab" |> Binding.cmdParam (fun o ->
-                    match o with
-                    | :? Tab as tabToClose -> CloseTab (Some tabToClose)
-                    | _ -> CloseTab None)
+                "Name" |> Binding.oneWay (fun (_, t) -> t.Name)
+                "Close" |> Binding.cmd (fun (_, t) -> CloseTab t.Id)
             ])
         "AddTab" |> Binding.cmd AddTab
         "SelectedTab" |> Binding.subModelSelectedItem("Tabs",
-            (fun m -> m.SelectedTab), (fun t -> SetSelectedTab t))
+            (fun m -> m.SelectedTab), (fun t -> SelectTab t))
     ]
 
 [<EntryPoint; STAThread>]
 let main argv =
-    Program.mkProgramWpf (fun () -> init) update bindings
+    Program.mkSimpleWpf (fun () -> init) update bindings
     |> Program.withConsoleTrace
     |> Program.runWindowWithConfig
         { ElmConfig.Default with LogConsole = true; Measure = true }
