ee834d83b72d796d11cd2daeb83b1640acd08f2c
KingKnecht <kingknecht@googlemail.com>
2019-10-15 17:47:58 +0200

    Close Tab by Tab whose button was pressed

--- TabsProblem/Program.fs
+++ TabsProblem/Program.fs
@@ -3,59 +3,70 @@ open Elmish
 open Elmish.WPF
 open MyMainWinodw
 
-
 let makeNameGenerator prefix =
-  let mutable intId = 0
-  fun () -> 
+    let mutable intId = 0
+    fun () ->
         intId <- (+) intId 1
-        (prefix <| intId)
+        prefix <| intId
 
 let createTabId = makeNameGenerator (sprintf "-%03i")
 
-type Tab = {
-    Name : string
+type Tab =
+    {
+        Name: string
     }
 
-type Model =  { 
-    Tabs: Tab list
-    SelectedTab : Tab option
-   }
+type Model =
+    {
+        Tabs: Tab list
+        SelectedTab: Tab option
+    }
 
-let init = 
+let init =
     {
         Tabs = []
         SelectedTab = None
-    }
-    , Cmd.none
+    }, Cmd.none
 
 type Msg =
-  | AddTab
-  | CloseTab
-  | CloseTab2 of Tab
-  | SetSelectedTab of Tab option
-    
+    | AddTab
+    | CloseTab of Tab option
+    | CloseTab2 of Tab
+    | SetSelectedTab of Tab option
+
 let update msg m =
-  match msg with
-  | AddTab -> {m with Tabs = (m.Tabs |> List.append [{Name = "Tab" + createTabId ()}])}, Cmd.none
-  | CloseTab -> match m.SelectedTab with
-                | Some t -> { m with SelectedTab = None }, Cmd.ofMsg (CloseTab2 t)
-                | None -> m, Cmd.none
-  | CloseTab2 tabToDelete -> {m with Tabs = m.Tabs |> List.filter ((<>) tabToDelete)}, Cmd.none
-  | SetSelectedTab t -> {m with SelectedTab = t}, Cmd.none
+    match msg with
+    | AddTab ->
+        { m with Tabs = m.Tabs |> List.append [ { Name = "Tab" + createTabId () } ] },
+            Cmd.none
+    | CloseTab tabToClose ->
+        match tabToClose with
+        | Some tabToClose -> { m with SelectedTab = None }, Cmd.ofMsg (CloseTab2 tabToClose)
+        | None -> m, Cmd.none
+    | CloseTab2 tabToClose ->
+        { m with Tabs = m.Tabs |> List.filter ((<>) tabToClose) }, Cmd.none
+    | SetSelectedTab t ->
+        { m with SelectedTab = t }, Cmd.none
 
-
-let bindings () : Binding<Model, Msg> list = [
-    "Tabs" |> Binding.subModelSeq((fun m -> m.Tabs),(fun s -> s), fun () -> [
-                                                                                "Name" |> Binding.oneWay(fun (_, e) -> e.Name)
-                                                                                "CloseTab" |> Binding.cmd(CloseTab)])
-    "AddTab" |> Binding.cmd(AddTab)
-    "SelectedTab" |> Binding.subModelSelectedItem("Tabs",(fun m -> m.SelectedTab),(fun t -> SetSelectedTab t))
-]   
+let bindings () : Binding<Model, Msg> list =
+    [
+        "Tabs" |> Binding.subModelSeq((fun m -> m.Tabs), (fun s -> s), fun () ->
+            [
+                "Tab" |> Binding.oneWay (fun (_, e) -> e)
+                "CloseTab" |> Binding.cmdParam (fun o ->
+                    match o with
+                    | :? Tab as tabToClose -> CloseTab (Some tabToClose)
+                    | _ -> CloseTab None)
+            ])
+        "AddTab" |> Binding.cmd AddTab
+        "SelectedTab" |> Binding.subModelSelectedItem("Tabs",
+            (fun m -> m.SelectedTab), (fun t -> SetSelectedTab t))
+    ]
 
 [<EntryPoint; STAThread>]
 let main argv =
-  Program.mkProgramWpf  (fun () -> init) update bindings
-  |> Program.withConsoleTrace
-  |> Program.runWindowWithConfig
-    { ElmConfig.Default with LogConsole = true; Measure = true }
-    (MainWindow())
+    Program.mkProgramWpf (fun () -> init) update bindings
+    |> Program.withConsoleTrace
+    |> Program.runWindowWithConfig
+        { ElmConfig.Default with LogConsole = true; Measure = true }
+        (MainWindow())
